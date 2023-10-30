e1d7664f23f505905cd518da1e7e8b698f42f4f7
Phioria <chris.baugh@gmail.com>
2023-10-30 08:41:27 -0400

    Add scripts to work with email and csv files

---
+++ utils/mailer.js
@@ -0,0 +1,96 @@
+const nodemailer = require('nodemailer');
+
+// TODO Update this function with a try catch block to catch errors
+const sendMail = async (recipientEmail, recipientFirstName, tokenLink) => {
+    // Define Transporter
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.forwardemail.net',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.MAILER_USER,
+            pass: process.env.MAILER_SECRET,
+        },
+    });
+
+    // Send mail with defined transport object
+    // TODO Change verify link to a frontend link for cors to work
+    const info = await transporter.sendMail({
+        from: '"Chris Baugh" <admin@phioria.com>', // Sender address
+        to: recipientEmail,
+        subject: 'Welcome to OmicsBase!',
+        html: `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>Document</title>
+        </head>
+        <body>
+            <h3>Hi ${recipientFirstName}!</h3>
+            <p> Thanks for signing up for <a href="https://omicsbase.com">OmicsBase!</a> We hope you enjoy this service.<br>
+            If you have any problems or suggestions, feel free to reach out to: chris.baugh@gmail.com<br>
+            Please click the link below to verify your account.</p>
+        
+            <a class="btn btn-primary" href=${tokenLink} role="button">Verify Account</a>
+        
+            <p><br>If the link above does not work, just copy and paste the link below into your browser.<br>
+            ${tokenLink}
+            </p>
+        </body>
+        </html>`,
+    });
+
+    // TODO Add logger functionality here
+    console.log('Message sent: %s', info.messageId);
+    return info;
+};
+
+const updatePasswordLink = async (recipientEmail, tokenLink) => {
+    // Define Transporter
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.forwardemail.net',
+        port: 465,
+        secure: true,
+        auth: {
+            user: process.env.MAILER_USER,
+            pass: process.env.MAILER_SECRET,
+        },
+    });
+
+    // Send mail with defined transport object
+    // TODO Change verify link to a frontend link for cors to work
+    const info = await transporter.sendMail({
+        from: '"Chris Baugh" <admin@phioria.com>', // Sender address
+        to: recipientEmail,
+        subject: 'Password Reset Request',
+        html: `<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+            <title>Document</title>
+        </head>
+        <body>
+            <h3>Hi ${recipientEmail}!</h3>
+            <p>We've received your request to reset your password. Please click the link below
+            to reset your password. The link is only valid for 20 minutes. If you did not request
+            this, then you can safely disregard this E-mail.</p>
+        
+            <a class="btn btn-primary" href=${tokenLink} role="button">Reset Password</a>
+        
+            <p><br>If the link above does not work, just copy and paste the link below into your browser.<br>
+            ${tokenLink}
+            </p>
+        </body>
+        </html>`,
+    });
+
+    // TODO Add logger functionality here
+    console.log('Message sent: %s', info.messageId);
+    return info;
+};
+
+module.exports = { sendMail, updatePasswordLink };
---
+++ utils/validateCsv.js
@@ -0,0 +1,56 @@
+const ROLES_LIST = require('../config/roles_list');
+
+const ALLOWED_CSV_HEADERS = [
+    'dataset',
+    'species',
+    'track_name',
+    'sequencing_type',
+    'file_location',
+    'mutant',
+    'tissue',
+    'sex',
+    'total_mapped',
+    'percent_aligned',
+    'percent_uniquely_mapped',
+    'author',
+    'cell_line',
+    'development_stage',
+    'project',
+    'paper',
+    'srr_id',
+    'notes',
+];
+
+const validHeader = (header) => {
+    return ALLOWED_CSV_HEADERS.includes(header.toLowerCase());
+};
+
+// This ensures that certain rows don't contain any empty data before submission
+const validateRow = (row, roles) => {
+    if (roles.includes(ROLES_LIST.Admin)) {
+        return row.dataset.length && row.species.length && row.sequencing_type.length && row.file_location.length && row.tissue.length;
+    } else {
+        // Check to see if the numeric cells are empty then check to see if you can strip a percent sign off them and force them to float. If they're NaN, return false
+        // These should always come in as strings to start with, so running .replace on them shouldn't throw an error...guess we'll see
+        const isTotalMappedNumeric = row.total_mapped.length && !isNaN(parseFloat(row.total_mapped.replace('%', '')));
+        const isPercentAlignedNumeric = row.percent_aligned.length && !isNaN(parseFloat(row.percent_aligned.replace('%', '')));
+        const isPercentUniquelyMappedNumeric = row.percent_uniquely_mapped.length && !isNaN(parseFloat(row.percent_uniquely_mapped.replace('%', '')));
+
+        return (
+            row.dataset.length &&
+            row.species.length &&
+            row.track_name.length &&
+            row.sequencing_type.length &&
+            row.file_location.length &&
+            row.mutant.length &&
+            row.tissue.length &&
+            row.sex.length &&
+            isTotalMappedNumeric &&
+            isPercentAlignedNumeric &&
+            isPercentUniquelyMappedNumeric &&
+            row.author.length
+        );
+    }
+};
+
+module.exports = { validHeader, validateRow, ALLOWED_CSV_HEADERS };
