2fa7a7a2b077fbaca3c5f72499b94c0ee9b92a22
Phioria <chris.baugh@gmail.com>
2023-10-30 08:41:02 -0400

    Add controller to generate new access tokens from a refresh token

---
+++ controllers/refreshTokenController.js
@@ -0,0 +1,36 @@
+const Users = require('../models').user;
+const jwt = require('jsonwebtoken');
+
+const handleRefreshToken = async (req, res) => {
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401);
+    //console.log(cookies.jwt);
+    const refreshToken = cookies.jwt;
+
+    const foundUser = await Users.findOne({
+        where: { refresh_token: refreshToken },
+    });
+    if (!foundUser) return res.sendStatus(403); // Forbidden
+
+    // Evaluate jwt
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+        if (err || foundUser.username !== decoded.username) {
+            return res.sendStatus(403);
+        }
+        const username = foundUser.username;
+        const roles = Object.values(foundUser.roles);
+        const accessToken = jwt.sign(
+            {
+                UserInfo: {
+                    username: decoded.username,
+                    roles: roles,
+                },
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '30s' } // 1800s
+        );
+        res.json({ username, roles, accessToken });
+    });
+};
+
+module.exports = { handleRefreshToken };
