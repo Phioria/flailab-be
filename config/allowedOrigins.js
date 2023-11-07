513aea439d05b234a082c03bd6befd9c069f4d41
Phioria <chris.baugh@gmail.com>
2023-11-07 13:23:30 -0500

    Change front-end url

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,4 @@
-const allowedOrigins = ['https://omicsbase.phioria.com'];
+//const allowedOrigins = ['https://omicsbase.phioria.com'];
+const allowedOrigins = ['https://main.d1qrft0fx9ek7j.amplifyapp.com/'];
 
 module.exports = allowedOrigins;
--- config/serverInfo.js
+++ config/serverInfo.js
@@ -1,3 +1,5 @@
-exports.FRONT_END_BASE_URL = 'https://omicsbase.phioria.com';
+//exports.FRONT_END_BASE_URL = 'https://omicsbase.phioria.com';
+exports.FRONT_END_BASE_URL = 'https://main.d1qrft0fx9ek7j.amplifyapp.com/';
+
 //exports.FRONT_END_BASE_URL = 'http://localhost';
 //exports.PORT = process.env.PORT || 5001;
